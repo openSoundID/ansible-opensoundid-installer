e126990615d5068b760c74ef58019431f5245155
jerome <lamourj@free.Fr>
2021-03-14 20:24:34 +0100

    add analyze.sh

---
+++ roles/install-opensoundid/files/database/update.sql
@@ -0,0 +1,25 @@
+update RECORD set ENABLED=FALSE where RMK like '%3__0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%3__0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%2__0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%2__0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%2kHz%';
+update RECORD set ENABLED=FALSE where RMK like '%19_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%19_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%18_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%18_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%17_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%17_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%16_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%16_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%15_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%15_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%14_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%14_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%13_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%13_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%12_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%12_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%11_0Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%11_0 Hz%';
+update RECORD set ENABLED=FALSE where RMK like '%noise%';
+
